a310e8eeea5af3709175619acb45fef1d0c01b62
frutafri <53392977+frutafri@users.noreply.github.com>
2019-07-29 22:02:06 -0400

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,7 +2,41 @@ board = {"tl": " ", "tm": " ", "tr": " ",
          "ml": " ", "mm": " ", "mr": " ",
          "bl": " ", "bm": " ", "br": " "}
 board_options = list(board.keys())
+board_values = list(board.values())
 
+tl = board_values[0]
+tm = board_values[1]
+tr = board_values[2]
+ml = board_values[3]
+mm = board_values[4]
+mr = board_values[5]
+bl = board_values[6]
+bm = board_values[7]
+br = board_values[8]
+
+def match(a,b,c):
+    if a == b:
+    else:
+        return False
+       
+def match_check():
+    if match(tl,tm,tr):
+        return True
+    if match(tl,mm,br):
+        return True
+    if match(tl,ml,bl):
+        return True
+    if match(tm,mm,bm):
+        return True
+    if match(br,bm,bl):
+        return True
+    if match(br,mr,tr):
+        return True
+    if match(ml,mm,mr):
+        return True
+    if match(bl,mm,tr):
+        return True
+    
 def print_board(board):
     print(board["tl"] + "|" + board["tm"] + "|" + board["tr"])
     print("-+-+-")
@@ -22,7 +56,11 @@ while not winner:
             turn = "O"
         else:
             turn = "X"
-        print_board(board)    
+        print_board(board)
+        
+        if match_check():
+            print("We have a winner!")
+            break
+
     else:
         continue
-            
